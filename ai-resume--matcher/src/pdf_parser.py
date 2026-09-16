f7f454f6951448b0efac6ecd3c41f85ed187e304
Gugan S <gugansrini17@gmail.com>
2026-09-17 00:08:29 +0530

    Improve PDF text extraction in pdf_parser.py
    Extracted Text Sucessfully to easily handle them for pre processing

--- ai-resume--matcher/src/pdf_parser.py
+++ ai-resume--matcher/src/pdf_parser.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader
-
+import re
 
 def extract_resume_text(pdf_path):
     reader = PdfReader(pdf_path)
@@ -7,9 +7,13 @@ def extract_resume_text(pdf_path):
 
     for page in reader.pages:
         text = page.extract_text()
-        if reader.pages!=None:
-            full_text+=text
+        if text is None:
+            continue
+        else:
+            full_text+=text + "\n"
     return full_text
+
+    
 
 extracted_text = extract_resume_text(r"D:\ai-resume--matcher\data\resumes\ml_resume.pdf")
 print(extracted_text)
